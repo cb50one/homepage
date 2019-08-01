d9d28e9e5a12b174ed9f5f202098c5b8adce1ac3
cb50one <txmalon0628@gmail.com>
2019-08-01 17:36:04 +0900

    add ga

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,4 +101,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  GA.tracker = Rails.application.credentials.tracking_ID
 end
